fab69886815343e6758c270c3c43cd6f112ee0fa
hungdv <danghungtb26@gmail.com>
2023-03-15 11:04:21 +0700

    feat: apollo gateway

---
+++ Dockerfile
@@ -0,0 +1,45 @@
+# Install dependencies only when needed
+FROM node:16-alpine AS deps
+# Check https://github.com/nodejs/docker-node/tree/b4117f9333da4138b03a546ec926ef50a31506c3#nodealpine to understand why libc6-compat might be needed.
+RUN apk add --no-cache libc6-compat
+WORKDIR /app
+
+# Install dependencies based on the preferred package manager
+COPY package.json yarn.lock* package-lock.json* pnpm-lock.yaml* ./
+RUN \
+  if [ -f yarn.lock ]; then yarn --frozen-lockfile; \
+  elif [ -f package-lock.json ]; then npm ci; \
+  elif [ -f pnpm-lock.yaml ]; then yarn global add pnpm && pnpm i --frozen-lockfile; \
+  else echo "Lockfile not found." && exit 1; \
+  fi
+
+# Rebuild the source code only when needed
+FROM node:16-alpine AS builder
+WORKDIR /app
+COPY --from=deps /app/node_modules ./node_modules
+COPY . .
+
+RUN yarn build:ncc
+
+FROM node:16-alpine AS runner
+WORKDIR /app
+
+ENV NODE_ENV production
+
+RUN addgroup --system --gid 1001 nodejs
+RUN adduser --system --uid 1001 corejs
+
+COPY --from=builder --chown=corejs:nodejs /app/dist ./
+
+USER corejs
+
+EXPOSE 3000
+
+ENV PORT 3000
+ENV MONGO_HOST=
+ENV MONGO_DB=dynamic-ui
+ENV MONGO_REPLICASET=
+ENV AUTHEN_URL=
+
+
+CMD ["node", "index.js"]
---
+++ src/index.ts
@@ -0,0 +1,121 @@
+import express, { Express } from 'express'
+import dotenv from 'dotenv'
+import morgan from 'morgan'
+import helmet from 'helmet'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+import { ApolloServer } from '@apollo/server'
+import http from 'http'
+import { ApolloGateway, IntrospectAndCompose, RemoteGraphQLDataSource } from '@apollo/gateway'
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer'
+import { expressMiddleware } from '@apollo/server/express4'
+
+morgan.token('graphql-query', req => {
+  // @ts-ignore
+  const { query, variables, operationName } = req.body
+  return `GRAPHQL: \nOperation Name: ${operationName} \nQuery: ${query} \nVariables: ${JSON.stringify(
+    variables,
+  )}`
+})
+
+dotenv.config()
+
+const main = async () => {
+  const app: Express = express()
+  const port = process.env.PORT
+  app.use(bodyParser.json())
+  app.use(bodyParser.urlencoded({ extended: true }))
+  app.use(helmet())
+
+  // app.use(morgan(':graphql-query'))
+
+  const envKeys = Object.keys(process.env)
+
+  const adminService = envKeys.filter(i => i.startsWith('ADMIN_SERVICE_') && process.env[i])
+
+  const coreService = envKeys.filter(i => i.startsWith('SERVICE_') && process.env[i])
+
+  app.use(cors({ origin: '*', methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'] }))
+
+  const httpServer = http.createServer(app)
+
+  if (coreService.length) {
+    const coreGateway = new ApolloGateway({
+      supergraphSdl: new IntrospectAndCompose({
+        subgraphs: coreService.map(i => ({
+          name: i,
+          url: process.env[i],
+        })),
+        pollIntervalInMs: 3000,
+        introspectionHeaders: h => h,
+      }),
+      buildService: ({ name, url }) => {
+        return new RemoteGraphQLDataSource({
+          url,
+          willSendRequest: ({ request, context }) => {
+            if (context.req?.headers) {
+              // eslint-disable-next-line no-restricted-syntax
+              for (const [headerKey, headerValue] of Object.entries(context.req.headers)) {
+                // @ts-ignore
+                if (headerKey && headerValue) request.http?.headers.set(headerKey, headerValue)
+              }
+            }
+          },
+        })
+      },
+    })
+
+    const adminServer = new ApolloServer({
+      gateway: coreGateway,
+      plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+    })
+    await adminServer.start()
+
+    app.use('/graphql/core', expressMiddleware(adminServer))
+
+    console.log(`⚡️[server]: Core Service is running at http://localhost:${port}/graphql/core `)
+  }
+
+  if (adminService.length) {
+    const adminGateway = new ApolloGateway({
+      supergraphSdl: new IntrospectAndCompose({
+        subgraphs: adminService.map(i => ({
+          name: i,
+          url: process.env[i],
+        })),
+        introspectionHeaders: h => h,
+        pollIntervalInMs: 3000,
+      }),
+      buildService: ({ name, url }) => {
+        return new RemoteGraphQLDataSource({
+          url,
+          willSendRequest: ({ request, context }) => {
+            if (context.req?.headers) {
+              // eslint-disable-next-line no-restricted-syntax
+              for (const [headerKey, headerValue] of Object.entries(context.req.headers)) {
+                // @ts-ignore
+                if (headerKey && headerValue) request.http?.headers.set(headerKey, headerValue)
+              }
+            }
+          },
+        })
+      },
+    })
+
+    const adminServer = new ApolloServer({
+      gateway: adminGateway,
+      plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+    })
+    await adminServer.start()
+
+    app.use('/graphql/admin', expressMiddleware(adminServer, { context: async r => r }))
+
+    console.log(`⚡️[server]: Admin Service is running at http://localhost:${port}/graphql/admin `)
+  }
+
+  httpServer.listen({ port }, () => {
+    console.log(`⚡️[server]: Server is running at http://localhost:${port} `)
+  })
+}
+
+main()
